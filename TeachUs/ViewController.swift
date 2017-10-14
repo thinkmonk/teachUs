d1a59d726a1ca1fd4db7f08934e4d493b9017ba6
gangarAnand <32612869+gangarAnand@users.noreply.github.com>
2017-10-14 18:34:06 +0530

    Add files via upload
    Initiall commit

---
+++ TeachUs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeachUs
+//
+//  Created by APPLE on 13/10/17.
+//  Copyright © 2017 TeachUs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
