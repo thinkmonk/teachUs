2fd91c1423e288cdde1d7c6d3e59bf7f88f05564
ios <ios@harshs-macbook-air.local>
2019-03-28 00:19:37 +0530

    Added extension to convert int to bool

---
+++ TeachUs/Extensions/MiscallenousExtensions.swift
@@ -0,0 +1,12 @@
+//
+//  MiscallenousExtensions.swift
+//  TeachUs
+//
+//  Created by ios on 3/28/19.
+//  Copyright © 2019 TeachUs. All rights reserved.
+//
+
+import Foundation
+extension Int {
+    var boolValue: Bool { return self != 0 }
+}
